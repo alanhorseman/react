0e1d8b63c18d6d5fe2b74401e3626cb6f67ce8c8
alan barbera <alanbarbera02@gmail.com>
2025-04-05 23:26:40 -0300

    style carrito

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -2,6 +2,8 @@ import { useContext, useState } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
+import "./Cart.css";
+
 const initialValuesUser = {
   name: "",
   email: "",
@@ -40,48 +42,65 @@ export const Cart = () => {
   return (
     <>
       <h1>Carrito</h1>
-      <div className="cart">
-        {products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.name}</h2>
-            <img src={product.image} width={200} alt={product.name} />
-            <h3>Precio: ${product.price}</h3>
-            <h3>Cantidad: {product.count}</h3>
-            <h3>Subtotal: ${product.price * product.count}</h3>
-          </div>
-        ))}
-      </div>
 
       <section>
-        <div>
-          <h3>Total: {total}</h3>
-          <button onClick={handleShoppingCart}>Comprar</button>
+        <div className="cardContainer">
+          {products.map((product) => (
+            <div key={product.id} className="card">
+              <h2>{product.name}</h2>
+              <div className="imgCardContainer">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="imgCard"
+                />
+              </div>
+              <div className="cardInfo">
+                <h3>Precio: ${product.price}</h3>
+                <h3>Cantidad: {product.count}</h3>
+                <h3>Subtotal: ${product.price * product.count}</h3>
+              </div>
+            </div>
+          ))}
         </div>
-        <form action="">
-          <label>Nombre</label>
-          <input
-            type="text"
-            name="name"
-            value={user.name}
-            onChange={handleChange}
-          />
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={user.email}
-            onChange={handleChange}
-          />
-          <label>Telefono</label>
-          <input
-            type="number"
-            name="phone"
-            value={user.phone}
-            onChange={handleChange}
-          />
-        </form>
+        <div className="infoForm">
+          
+          <form action="">
+            <div>
+              <label>Nombre</label>
+              <input
+                type="text"
+                name="name"
+                value={user.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label>Telefono</label>
+              <input
+                type="number"
+                name="phone"
+                value={user.phone}
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+          <div>
+            <h3>Total: {total}</h3>
+            <button onClick={handleShoppingCart}>Comprar</button>
+          </div>
+        </div>
       </section>
-      <button onClick={onRemove}>Vaciar carrito</button>
+      <button onClick={onRemove} className="buttonRemove">Vaciar carrito</button>
     </>
   );
 };
