6411c5880becca0eabfcf550047af0cc52dc33eb
alan barbera <alanbarbera02@gmail.com>
2025-04-06 00:05:04 -0300

    style cards

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,18 +1,26 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "./MyCard.css";
 
 export const MyCard = ({ producto }) => (
-  <Card key={producto.id} style={{ width: "18rem" }}>
-    <Card.Img variant="top" src={producto.image} />
-    <Card.Body>
-      <Card.Title>{producto.name}</Card.Title>
-      <Card.Text>{producto.description}</Card.Text>
-      <Card.Text>Stock: {producto.stock}</Card.Text>
-      <Card.Text>Precio: {producto.price}</Card.Text>
-      <Link to={`/detail/${producto.id}`} >
-        <Button variant="primary">Ver detalles</Button>
-      </Link>
-    </Card.Body>
-  </Card>
+  <Container className="cardContainerHome">
+    <Card key={producto.id} style={{ width: "18rem" }}>
+      <Card.Img className="imgCarddHome" variant="top" src={producto.image} />
+      <Card.Body>
+        <Container className="infoCardHome" >
+          <Card.Title className="cardTitleHome">{producto.name}</Card.Title>
+          <Card.Text>{producto.description}</Card.Text>
+          <Card.Text>Stock: {producto.stock}</Card.Text>
+          <Card.Text>Precio: {producto.price}</Card.Text>
+        </Container>
+        <Container className="btnContainerHome">
+          <Link to={`/detail/${producto.id}`}>
+            <Button className="btnCardHome" variant="primary">Ver detalles</Button>
+          </Link>
+        </Container>
+      </Card.Body>
+    </Card>
+  </Container>
 );
